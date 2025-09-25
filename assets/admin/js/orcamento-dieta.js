e55a79278d7a12ca39df883be510f81a83fe2094
Márcio José Mariano <marciojmariano@gmail.com>
2025-09-25 20:20:54 -0300

    Atualização final

--- assets/admin/js/orcamento-dieta.js
+++ assets/admin/js/orcamento-dieta.js
@@ -1,29 +1,12 @@
 let ingredientes = carregarIngredientesLocalStorage();
 
-// [
-//   {
-//     "id": "54b2bf8e-3289-4df3-8fb9-7003b3b88efc",
-//     "nome": "Grão de Bico",
-//     "tipo": "leguminosa",
-//     "valor": "15"
-//   },
-//   {
-//     "id": "255eb2b2-a6cf-4b81-9cc4-80f76c95c94d",
-//     "nome": "Arroz Negro",
-//     "tipo": "carboidrato",
-//     "valor": "10"
-//   },
-//   {
-//     "id": "b04b5c6b-9cbb-47b2-87b2-88a541a94877",
-//     "nome": "Salmão",
-//     "tipo": "proteina",
-//     "valor": "100"
-//   }
-// ]
+
 // Tabela de preços dos ingredientes
 const ingredientesCarboidratos = ingredientes.filter(ingrediente => ingrediente.tipo === "carboidrato");
 const ingredientesProteinas = ingredientes.filter(proteina => proteina.tipo === "proteina");
 const ingredientesLegumes = ingredientes.filter(legume => legume.tipo === "legumes");
+const ingredientesLeguminosas = ingredientes.filter(leguminosa => leguminosa.tipo === "leguminosa");
+
 //const ingredientesLeguminosa = ingredientes.filter(leguminosa => leguminosa.tipo === "leguminosa");
 
 const selectIngredientes = document.getElementById("ingredientes");
@@ -34,6 +17,7 @@ let totalPreco = 0;
 
 // Função para adicionar ingrediente à tabela
 function adicionarIngrediente(tipoIngrediente) {
+    debugger
     const selecaoTipoIngrediente = document.getElementById(tipoIngrediente);
     const quantidadeInformada = document.getElementById(`quantidade-${tipoIngrediente}`);
     const ingredienteSelecionado = selecaoTipoIngrediente.value;
@@ -101,7 +85,13 @@ function definirIngredientes($event) {
         criarOptions(ingredientesCarboidratos);
     } else if (tipo === "proteina") {
         criarOptions(ingredientesProteinas);
+    } else if (tipo === "legume") {
+        criarOptions(ingredientesLegumes);
+    } else if (tipo === "leguminosa"){
+        criarOptions(ingredientesLeguminosas);
     }
+
+    
 
 }
 
