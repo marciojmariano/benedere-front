2c87f831e3e40c631960740e0ad58e41f52251cb
Márcio José Mariano <marciojmariano@gmail.com>
2025-09-28 08:37:43 -0300

    Gravação do orçamento da storage

--- assets/admin/js/orcamento-dieta.js
+++ assets/admin/js/orcamento-dieta.js
@@ -1,4 +1,5 @@
 let ingredientes = carregarIngredientesLocalStorage();
+let orcamentos = carregarOrcamentosLocalStorage();
 
 
 // Tabela de preços dos ingredientes
@@ -10,58 +11,72 @@ const ingredientesLeguminosas = ingredientes.filter(leguminosa => leguminosa.tip
 //const ingredientesLeguminosa = ingredientes.filter(leguminosa => leguminosa.tipo === "leguminosa");
 
 const selectIngredientes = document.getElementById("ingredientes");
+const inputQuantidade = document.getElementById(`quantidade-ingredientes`);
+
+const totalQuantidadeElemento = document.getElementById("totalQuantidade");
+const totalPrecoElemento = document.getElementById("totalPreco");
+const tabelaOrcamentos = document.getElementById("tabelaItens").querySelector("tbody");
 
 // Variáveis para armazenar os totais
 let totalQuantidade = 0;
 let totalPreco = 0;
 
 // Função para adicionar ingrediente à tabela
-function adicionarIngrediente(tipoIngrediente) {
+function adicionarIngrediente() {
     debugger
-    const selecaoTipoIngrediente = document.getElementById(tipoIngrediente);
-    const quantidadeInformada = document.getElementById(`quantidade-${tipoIngrediente}`);
-    const ingredienteSelecionado = selecaoTipoIngrediente.value;
-    const quantidade = (parseInt(quantidadeInformada.value, 10)) / 1000;
+    const tipoRefeicao = document.querySelector("input[name='tipoRefeicao']:checked").value;
+    const ingredienteSelecionado = selectIngredientes.value;
+    const quantidade = (parseInt(inputQuantidade.value, 10)) / 1000;
+    const ingrediente = ingredientes.filter(ingrediente => ingrediente.id === ingredienteSelecionado)[0];
 
     if (!ingredienteSelecionado || isNaN(quantidade) || quantidade <= 0) {
         alert("Por favor, selecione um ingrediente e informe uma quantidade válida.");
         return;
     }
 
-    if (!precosIngredientes[ingredienteSelecionado]) {
+    if (ingrediente === undefined) {
         alert("Ingrediente selecionado inválido!");
         return;
     }
-
-    const ingrediente = precosIngredientes[ingredienteSelecionado];
-    const precoUnitario = ingrediente.preco;
+    const precoUnitario = ingrediente.valor;
     const precoTotal = precoUnitario * quantidade;
+    salvarLocalStorage(tipoRefeicao, ingrediente, quantidade, precoTotal);
 
-    totalQuantidade += quantidade; //Coisa nova que aprendi +=
-    totalPreco += precoTotal;
+    listarIngredientesTabela();
+    atualizarTotais(quantidade, precoTotal);
 
-    const tabelaIngredientes = document.getElementById("tabelaItens").querySelector("tbody");
+    selectIngredientes.value = "";
+    inputQuantidade.value = 0;
+}
+
+function salvarLocalStorage(tipoRefeicao, ingrediente, quantidade, precoTotal) {
+    let orcamento = {
+        refeicao: tipoRefeicao,
+        quantidade: quantidade,
+        ingrediente: ingrediente,
+        precoTotal: precoTotal
+    }
+    orcamentos.push(orcamento);
+    localStorage.setItem("orcamentos", JSON.stringify(orcamentos));
+}
+
+function criarLinhaTabela(tabelaOrcamentos, orcamento) {
     const novaLinha = document.createElement("tr");
 
     novaLinha.innerHTML = `
-        <td>${ingrediente.nome}</td>
-        <td>${quantidade}</td>
-        <td>R$ ${precoUnitario.toFixed(2).replace(".", ",")}</td>
-        <td>R$ ${precoTotal.toFixed(2).replace(".", ",")}</td>
+        <td>${orcamento.refeicao}</td>
+        <td>${orcamento.ingrediente.nome}</td>
+        <td>${orcamento.quantidade}</td>
+        <td>R$ ${orcamento.ingrediente.valor.toFixed(2).replace(".", ",")}</td>
+        <td>R$ ${orcamento.precoTotal.toFixed(2).replace(".", ",")}</td>
     `;
-
-    tabelaIngredientes.appendChild(novaLinha);
-
-    atualizarTotais();
-
-    selecaoTipoIngrediente.value = "";
-    quantidadeInformada.value = 0;
+    tabelaOrcamentos.appendChild(novaLinha);
 }
 
 // Função para atualizar os totais na interface
-function atualizarTotais() {
-    const totalQuantidadeElemento = document.getElementById("totalQuantidade");
-    const totalPrecoElemento = document.getElementById("totalPreco");
+function atualizarTotais(quantidade, precoTotal) {
+    totalQuantidade += quantidade; //Coisa nova que aprendi +=
+    totalPreco += precoTotal;
 
     totalQuantidadeElemento.textContent = totalQuantidade.toFixed(3).replace(".", ",");
     totalPrecoElemento.textContent = `R$ ${totalPreco.toFixed(2).replace(".", ",")}`;
@@ -74,9 +89,17 @@ function carregarIngredientesLocalStorage() {
     }
     let ingredientes = JSON.parse(ingredientesLocalStorage)
     return ingredientes;
-
-
 }
+
+function carregarOrcamentosLocalStorage() {
+    let orcamentosLocalStorage = localStorage.getItem("orcamentos");
+    if (orcamentosLocalStorage == null) {
+        return []
+    }
+    let orcamentos = JSON.parse(orcamentosLocalStorage)
+    return orcamentos;
+}
+
 
 function definirIngredientes($event) {
     //debugger
@@ -87,19 +110,28 @@ function definirIngredientes($event) {
         criarOptions(ingredientesProteinas);
     } else if (tipo === "legume") {
         criarOptions(ingredientesLegumes);
-    } else if (tipo === "leguminosa"){
+    } else if (tipo === "leguminosa") {
         criarOptions(ingredientesLeguminosas);
     }
-
-    
-
 }
 
 function criarOptions(ingredientes) {
     //<option value="arroz">Arroz</option>
     selectIngredientes.innerHTML = "<option disabled selected>-- Selecione um ingrediente --</option>";
     ingredientes.forEach(ingrediente => {
-        selectIngredientes.innerHTML += `<option value="arroz">${ingrediente.nome}</option>`
-
+        selectIngredientes.innerHTML += `<option value="${ingrediente.id}">${ingrediente.nome}</option>`
     });
 }
+
+function listarIngredientesTabela() {
+    tabelaOrcamentos.innerHTML = "";
+
+    orcamentos.forEach(orcamento => {
+        criarLinhaTabela(tabelaOrcamentos, orcamento);
+    });
+}
+
+// Carrega os ingredientes na tabela ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+    listarIngredientesTabela();
+});
